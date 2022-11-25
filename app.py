21f39f7efa3529ba6a4cde7914e70ed97e155494
Robson Ferreira <115955245+RobsonFe@users.noreply.github.com>
2022-11-25 10:23:21 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+print("\t\tTabela de Cadastro\n")
+
+n = int(input("Quantas pessoas serão cadastradas ? "))
+
+lista = [0 for x in range (n)]
+
+for i in range(0,n):
+    
+    nome = input("Nome completo: ")
+    lista[i] = nome
+    idade = int (input("Qual a sua idade ? "))
+    lista[i] = idade
+    profissao = input("Qual a sua profissão ? ")
+    lista[i] = profissao
+    sexo = input ("Qual o sexo ? (F/M) ")
+    lista[i] = sexo
+    salario = float(input("Qual o seu salario ? "))
+    lista[i] = salario 
+
+print()
+
+print("\t\tPessoas Cadastradas \n")
+for i in range(0,n):
+    print (lista[i])
